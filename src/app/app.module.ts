44688e29165dd1ca44b6f4e523a0f80489f8a12a
desh200 <umesh.deshmukh35@gmail.com>
2021-12-21 10:32:51 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home.component';
 import { NotFoundComponent } from './notfound.component';
 import { GitHubComponent } from './github.component';
+import { GitHubUserComponent } from './githubuser.component';
 import { routing } from './app.routing';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule, ReactiveFormsModul HomeComponent, NotFoundComponent,GitHubComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule, ReactiveFormsModul HomeComponent, NotFoundComponent,GitHubComponent,GitHubUserComponent],
   declarations: [AppComponent, HelloComponent],
   bootstrap: [AppComponent],
 })
