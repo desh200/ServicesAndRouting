b616577e14fa2e9641e2eace955f603dab491257
desh200 <umesh.deshmukh35@gmail.com>
2021-12-21 10:42:59 +0530

    1 file modified

---
+++ src/app/githubuser.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  template: `
+  <h1>User Login: {{login}}</h1>
+  `,
+})
+export class GitHubUserComponent {}
