ff40e3b636ed02439eca6e0c3452d34bd85a4763
desh200 <umesh.deshmukh35@gmail.com>
2021-12-21 09:42:46 +0530

    2 files modified, 1 added

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,9 +5,11 @@ import { HomeComponent } from './home.component';
 import { NotFoundComponent } from './notfound.component';
 
 import { GitHubComponent } from './github.component';
+import { GitHubUserComponent } from './github.component';
 
 export const routing = RouterModule.forRoot([
   { path: '', component: HomeComponent },
   { path: 'GitHub', component: GitHubComponent },
+  { path: 'GitHub/user/:login/:score', component: GitHubUserComponent },
   { path: '**', component: NotFoundComponent },
 ]);
