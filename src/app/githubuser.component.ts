01cef4af2a3ef41254c4c1a9a7f4daa4adde3f61
desh200 <umesh.deshmukh35@gmail.com>
2021-12-26 11:37:08 +0530

    2 files modified

--- src/app/githubuser.component.ts
+++ src/app/githubuser.component.ts
@@ -13,4 +13,11 @@ export class GitHubUserComponent {
   subscription;
 
   constructor(private _route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.subscription = this._route.params.subscribe((params) => {
+      this.login = params['login'];
+      this.score = params['score'];
+    });
+  }
 }
