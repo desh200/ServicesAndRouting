b61ccb412c0ec66cd2d47a12c88052abb8804ff8
desh200 <umesh.deshmukh35@gmail.com>
2021-12-31 08:29:50 +0530

    1 file modified

--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -1,20 +1,9 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
 
 @Injectable()
 export class LoginService {
-  isLoggedin = false;
-
   login(username, password) {
-    if (username === 'json' && password === '123') {
-      this.isLoggedin = true;
-    } else {
-      this.isLoggedin = false;
-    }
-  }
-
-  logout{
-    this.isLoggedin = false;
-    return  this.isLoggedin;
+    if (username == 'jason' && password == '123') return true;
+    else return false;
   }
 }
