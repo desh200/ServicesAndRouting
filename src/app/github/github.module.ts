f10bb2b93fe88f436447282b9f34c21879a3e2ac
desh200 <umesh.deshmukh35@gmail.com>
2022-01-02 14:35:24 +0530

    1 file modified, 3 added, 4 deleted

--- src/app/github/github.module.ts
+++ src/app/github/github.module.ts
@@ -1,6 +1,4 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@aangular/router';
 import { RouterModule } from '@aangular/router';
