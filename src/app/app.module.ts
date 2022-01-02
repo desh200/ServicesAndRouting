b9153a0477f4a3bc762a46de8dbf6cfc941b15da
desh200 <umesh.deshmukh35@gmail.com>
2022-01-02 15:45:12 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home.component';
 import { NotFoundComponent } from './notfound.component';
 import { LoginModule } from './login/login.module';
 import { GitHubModule } from './github/github.module';
-import { RouterModule, Routes } from '@angular/router';
+import { routing } from './app.routing';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    RouterModule,
-    FormsModule,
-    HttpClientModule,
-    LoginModule,
-    GitHubModule,
-    RouterModule,
-  ],
-  declarations: [
-    AppComponent,
-    HelloComponent,
-    HomeComponent,
-    NotFoundComponent,
-  ],
+  imports: [BrowserModule, LoginModule, GitHubModule, routing],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
