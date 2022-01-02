01a57c58ba3bce454b8559aa2e2aaedd1bcec35e
desh200 <umesh.deshmukh35@gmail.com>
2022-01-02 14:51:27 +0530

    7 files added, 7 deleted

---
+++ src/app/login/login.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './login.component';
+import { LoginService } from './login.service';
+import { PreventUnsavedCangesGuard } from './prevent-unsaved-changes-guard.service';
+
+import { AuthGuard } from './auth-guard.service';
+
+@NgModule({
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  declarations: [LoginComponent],
+  providers: [
+    AuthGuard, // <------------ Include here
+    LoginService,
+    PreventUnsavedCangesGuard,
+  ],
+})
+export class AppModule {}
